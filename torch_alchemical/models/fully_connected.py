6f4051061f062a4ed62b5e50c2d9cdc90a0f90cb
Arslan Mazitov <arslan.mazitov@epfl.ch>
2023-05-19 15:23:05 +0200

    Revorked Fully Connected Model

--- torch_alchemical/models/fully_connected.py
+++ torch_alchemical/models/fully_connected.py
@@ -1,38 +1,57 @@
+from typing import Union
+
 import equistore
+import numpy as np
 import torch
 
-from torch_alchemical.nn import Linear, PowerSpectrumFeatures, SiLU
+from torch_alchemical.nn import (
+    Linear,
+    PowerSpectrumFeatures,
+    RadialSpectrumFeatures,
+    SiLU,
+)
 
 
 class FCNN(torch.nn.Module):
     def __init__(
         self,
-        hidden_size,
-        output_size,
-        unique_numbers,
-        cutoff_radius,
-        cutoff_energy,
-        num_pseudo_species,
-        device=None,
+        hidden_sizes: int,
+        output_size: int,
+        unique_numbers: Union[list, np.ndarray],
+        cutoff: float,
+        basis_cutoff_radial_spectrum: float,
+        basis_cutoff_power_spectrum: float,
+        num_pseudo_species: int = None,
+        device: torch.device = None,
     ):
         super().__init__()
         self.composition_layer = torch.nn.Linear(len(unique_numbers), output_size)
-        self.features_layer = PowerSpectrumFeatures(
-            unique_numbers, cutoff_radius, cutoff_energy, num_pseudo_species, device
+        self.rs_features_layer = RadialSpectrumFeatures(
+            unique_numbers, cutoff, basis_cutoff_radial_spectrum, device
         )
-        input_size = self.features_layer.num_features
-        self.linear1 = Linear(input_size, hidden_size)
-        self.linear2 = Linear(hidden_size, hidden_size)
-        self.linear3 = Linear(hidden_size, output_size)
+        self.ps_features_layer = PowerSpectrumFeatures(
+            unique_numbers,
+            cutoff,
+            basis_cutoff_power_spectrum,
+            num_pseudo_species,
+            device,
+        )
+        rs_input_size = self.rs_features_layer.num_features
+        ps_input_size = self.ps_features_layer.num_features
+        input_size = rs_input_size + ps_input_size
+        layer_size = [input_size] + hidden_sizes + [output_size]
+        layers = []
+        for layer_index in range(1, len(layer_size)):
+            layers.append(Linear(layer_size[layer_index - 1], layer_size[layer_index]))
+            layers.append(SiLU())
+        self.nn = torch.nn.Sequential(*layers)
 
     def forward(self, batch, training=True):
         energies = self.composition_layer(batch.composition)
-        x = self.features_layer(batch)
-        x = self.linear1(x)
-        x = SiLU()(x)
-        x = self.linear2(x)
-        x = SiLU()(x)
-        x = self.linear3(x)
+        rs = self.rs_features_layer(batch)
+        ps = self.ps_features_layer(batch)
+        x = equistore.join((rs, ps), axis='properties')
+        x = self.nn(x)
         energies += (
             equistore.sum_over_samples(x.keys_to_samples("a_i"), ["center", "a_i"])
             .block()
