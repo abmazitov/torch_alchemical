821554673f8313aec94febd5e64d5da559a2a40b
Arslan Mazitov <arslan.mazitov@epfl.ch>
2023-07-20 17:04:43 +0200

    Added additional functionality for DataListLoader

--- tests/test_torch_spex_compatibility.py
+++ tests/test_torch_spex_compatibility.py
@@ -2,10 +2,11 @@ from ase.io import read
 import json
 import torch
 import numpy as np
-from torch_alchemical.utils import convert_batch_to_torch_spex_dict
+from torch_alchemical.utils import get_torch_spex_dict, get_torch_spex_dict_from_batch
 from torch_alchemical.data import AtomisticDataset
 from torch_alchemical.transforms import NeighborList
 from torch_geometric.loader import DataLoader as PyGDataLoader
+from torch_geometric.loader import DataListLoader
 from torch.utils.data import DataLoader
 from torch_spex.structures import InMemoryDataset, TransformerNeighborList, collate_nl
 from torch_spex.spherical_expansions import SphericalExpansion
@@ -29,10 +30,12 @@ class TestTorchSpexCompatibility:
         hypers = json.load(f)
     transforms = [NeighborList(cutoff_radius=hypers["cutoff radius"])]
     dataset = AtomisticDataset(
-        frames, target_properties=["energies", "forces"], transforms=transforms
+        frames, target_properties=["energies"], transforms=transforms
     )
     dataloader = PyGDataLoader(dataset, batch_size=len(frames), shuffle=False)
+    datalistloader = DataListLoader(dataset, batch_size=len(frames), shuffle=False)
     batch = next(iter(dataloader))
+    batch_list = next(iter(datalistloader))
 
     spex_transformers = [TransformerNeighborList(cutoff=hypers["cutoff radius"])]
     spex_dataset = InMemoryDataset(frames, spex_transformers)
@@ -43,8 +46,29 @@ class TestTorchSpexCompatibility:
     spex_batch.pop("positions")
     spex_batch.pop("cell")
 
+    def test_data_list_to_torch_spex_dict_conversion(self):
+        positions = [data.pos for data in self.batch_list]
+        cells = [data.cell for data in self.batch_list]
+        numbers = [data.numbers for data in self.batch_list]
+        edge_index = [data.edge_index for data in self.batch_list]
+        edge_shift = [data.edge_shift for data in self.batch_list]
+        data_list_dict = get_torch_spex_dict(
+            positions, cells, numbers, edge_index, edge_shift
+        )
+        assert data_list_dict.keys() == self.spex_batch.keys()
+        for key in data_list_dict.keys():
+            obj = data_list_dict[key]
+            target = self.spex_batch[key]
+            if obj.dim() == target.dim() == 1:
+                assert torch.equal(obj, target)
+            elif obj.dim() == target.dim() == 2:
+                obj_content, obj_counts = obj.unique(dim=0, return_counts=True)
+                target_content, target_counts = target.unique(dim=0, return_counts=True)
+                assert torch.equal(obj_content, target_content)
+                assert torch.equal(obj_counts, target_counts)
+
     def test_batch_to_torch_spex_dict_conversion(self):
-        batch_dict = convert_batch_to_torch_spex_dict(self.batch)
+        batch_dict = get_torch_spex_dict_from_batch(self.batch)
         assert batch_dict.keys() == self.spex_batch.keys()
         for key in batch_dict.keys():
             obj = batch_dict[key]
@@ -58,11 +82,23 @@ class TestTorchSpexCompatibility:
                 assert torch.equal(obj_counts, target_counts)
 
     def test_spherical_expansion_coefficients(self):
-        batch_dict = convert_batch_to_torch_spex_dict(self.batch)
+        batch_dict = get_torch_spex_dict_from_batch(self.batch)
+
+        positions = [data.pos for data in self.batch_list]
+        cells = [data.cell for data in self.batch_list]
+        numbers = [data.numbers for data in self.batch_list]
+        edge_index = [data.edge_index for data in self.batch_list]
+        edge_shift = [data.edge_shift for data in self.batch_list]
+        data_list_dict = get_torch_spex_dict(
+            positions, cells, numbers, edge_index, edge_shift
+        )
         calculator = SphericalExpansion(
             hypers=self.hypers, all_species=self.all_species, device=self.device
         )
         with torch.no_grad():
-            tm = calculator.forward(**batch_dict)
+            batch_tm = calculator.forward(**batch_dict)
+            data_tm = calculator.forward(**data_list_dict)
             spex_tm = calculator.forward(**self.spex_batch)
-        assert equistore.operations.allclose(tm, spex_tm, atol=1e-5, rtol=1e-5)
+
+        assert equistore.operations.allclose(batch_tm, spex_tm, atol=1e-5, rtol=1e-5)
+        assert equistore.operations.allclose(data_tm, spex_tm, atol=1e-5, rtol=1e-5)
--- torch_alchemical/utils/__init__.py
+++ torch_alchemical/utils/__init__.py
@@ -1,13 +1,14 @@
 from .atomic_numbers import get_list_of_unique_atomic_numbers
-from .convert_batch_to_torch_spex_dict import convert_batch_to_torch_spex_dict
 from .get_cartesian_vectors import get_cartesian_vectors
 from .get_species_coupling_matrix import get_species_coupling_matrix
 from .get_target_properties import get_target_properties
+from .get_torch_spex_dict import get_torch_spex_dict, get_torch_spex_dict_from_batch
 
 __all__ = [
     "get_list_of_unique_atomic_numbers",
     "get_cartesian_vectors",
     "get_target_properties",
     "get_species_coupling_matrix",
-    "convert_batch_to_torch_spex_dict",
+    "get_torch_spex_dict",
+    "get_torch_spex_dict_from_batch",
 ]
