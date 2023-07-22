40d479fada55e2f56790136f6b08073f80b05985
Arslan Mazitov <arslan.mazitov@epfl.ch>
2023-07-22 16:05:18 +0200

    Cleanup and rework of the AlchemicalModel

--- torch_alchemical/models/alchemical_model.py
+++ torch_alchemical/models/alchemical_model.py
@@ -1,15 +1,15 @@
-from typing import Union
-
 import equistore
 import numpy as np
 import torch
 
+from typing import Union
+
 from torch_alchemical.nn import (
     Linear,
     PowerSpectrumFeatures,
-    RadialSpectrumFeatures,
     SiLU,
 )
+from torch_alchemical.utils import get_compositions_from_numbers
 
 
 class AlchemicalModel(torch.nn.Module):
@@ -19,16 +19,13 @@ class AlchemicalModel(torch.nn.Module):
         output_size: int,
         unique_numbers: Union[list, np.ndarray],
         cutoff: float,
-        basis_cutoff_radial_spectrum: float,
         basis_cutoff_power_spectrum: float,
         num_pseudo_species: int = None,
         device: torch.device = None,
     ):
         super().__init__()
+        self.unique_numbers = unique_numbers
         self.composition_layer = torch.nn.Linear(len(unique_numbers), output_size)
-        self.rs_features_layer = RadialSpectrumFeatures(
-            unique_numbers, cutoff, basis_cutoff_radial_spectrum, device
-        )
         self.ps_features_layer = PowerSpectrumFeatures(
             unique_numbers,
             cutoff,
@@ -36,9 +33,7 @@ class AlchemicalModel(torch.nn.Module):
             num_pseudo_species,
             device,
         )
-        rs_input_size = self.rs_features_layer.num_features
         ps_input_size = self.ps_features_layer.num_features
-        self.rs_linear = Linear(rs_input_size, output_size)
         self.ps_linear = Linear(ps_input_size, output_size)
         layer_size = [ps_input_size] + hidden_sizes
         layers = []
@@ -48,17 +43,23 @@ class AlchemicalModel(torch.nn.Module):
         layers.append(Linear(layer_size[-1], output_size))
         self.nn = torch.nn.Sequential(*layers)
 
-    def forward(self, batch, training=True):
-        energies = self.composition_layer(batch.composition)
-        rs = self.rs_features_layer(batch)
-        ps = self.ps_features_layer(batch)
-        rsl = self.rs_linear(rs)
-        psl = self.ps_linear(ps)
-        energies += (
-            equistore.sum_over_samples(rsl.keys_to_samples("a_i"), ["center", "a_i"])
-            .block()
-            .values
+    def forward(
+        self,
+        positions: list[torch.Tensor],
+        cells: list[torch.Tensor],
+        numbers: list[torch.Tensor],
+        edge_indices: list[torch.Tensor],
+        edge_shifts: list[torch.Tensor],
+        training=True,
+    ):
+        compositions = torch.stack(
+            get_compositions_from_numbers(numbers, self.unique_numbers)
         )
+        energies = self.composition_layer(compositions)
+        ps = self.ps_features_layer(
+            positions, cells, numbers, edge_indices, edge_shifts
+        )
+        psl = self.ps_linear(ps)
         energies += (
             equistore.sum_over_samples(psl.keys_to_samples("a_i"), ["center", "a_i"])
             .block()
@@ -70,11 +71,4 @@ class AlchemicalModel(torch.nn.Module):
             .block()
             .values
         )
-        forces = -torch.autograd.grad(
-            energies,
-            batch.pos,
-            grad_outputs=torch.ones_like(energies),
-            create_graph=training,
-            retain_graph=training,
-        )[0]
-        return energies, forces
+        return energies
