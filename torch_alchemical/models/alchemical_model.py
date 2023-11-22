Fixed a bug with compositions calculation

torch_alchemical/models/alchemical_model.py
@@ -53,12 +53,12 @@ class AlchemicalModel(torch.nn.Module):
         ps_input_size = self.ps_features_layer.num_features
         if self.normalize:
             self.layer_norm = LayerNorm(ps_input_size)
-            combination_matrix = (
-                self.ps_features_layer.spex_calculator.vector_expansion_calculator.radial_basis_calculator.combination_matrix.linear_layer.weight
+            self.contraction_layer = (
+                self.ps_features_layer.spex_calculator.vector_expansion_calculator.radial_basis_calculator.combination_matrix.linear_layer
             )
         else:
-            combination_matrix = (
-                self.ps_features_layer.spex_calculator.vector_expansion_calculator.radial_basis_calculator.combination_matrix.weight
+            self.contraction_layer = (
+                self.ps_features_layer.spex_calculator.vector_expansion_calculator.radial_basis_calculator.combination_matrix
             )
         layer_size = [ps_input_size] + hidden_sizes
         layers = []
@@ -66,7 +66,7 @@ class AlchemicalModel(torch.nn.Module):
             layers.append(
                 AlchemicalContraction(
                     unique_numbers=unique_numbers,
-                    contraction_matrix=combination_matrix,
+                    contraction_matrix=self.contraction_layer.weight,
                     in_features=layer_size[layer_index - 1],
                     out_features=layer_size[layer_index],
                     bias=False,
@@ -76,7 +76,7 @@ class AlchemicalModel(torch.nn.Module):
         layers.append(
             AlchemicalContraction(
                 unique_numbers=unique_numbers,
-                contraction_matrix=combination_matrix,
+                contraction_matrix=self.contraction_layer.weight,
                 in_features=layer_size[-1],
                 out_features=output_size,
                 bias=False,

torch_alchemical/tools/train/initialize.py
@@ -10,9 +10,14 @@ def initialize_composition_layer_weights(model, datamodule, trainable=False):
     dataset = datamodule.train_dataset
     composition_layer = model.composition_layer
     numbers = torch.cat([data.numbers for data in dataset])
-    ptr = torch.cumsum(torch.tensor([0] + [data.num_nodes for data in dataset]), dim=0)
+    batch = torch.cat(
+        [
+            torch.repeat_interleave(torch.tensor([i]), data.num_nodes)
+            for i, data in enumerate(dataset)
+        ]
+    )
     compositions = torch.stack(
-        get_compositions_from_numbers(numbers, datamodule.unique_numbers, ptr)
+        get_compositions_from_numbers(numbers, datamodule.unique_numbers, batch)
     )
     bias = composition_layer.bias is not None
     if bias:
@@ -42,9 +47,14 @@ def initialize_energies_forces_scale_factor(
     dataset = datamodule.train_dataset
     composition_layer = model.composition_layer
     numbers = torch.cat([data.numbers for data in dataset])
-    ptr = torch.cumsum(torch.tensor([0] + [data.num_nodes for data in dataset]), dim=0)
+    batch = torch.cat(
+        [
+            torch.repeat_interleave(torch.tensor([i]), data.num_nodes)
+            for i, data in enumerate(dataset)
+        ]
+    )
     compositions = torch.stack(
-        get_compositions_from_numbers(numbers, datamodule.unique_numbers, ptr)
+        get_compositions_from_numbers(numbers, datamodule.unique_numbers, batch)
     )
     energies = torch.cat([data.energies.view(1, -1) for data in dataset], dim=0)
     composition_energies = composition_layer(compositions)
@@ -62,9 +72,14 @@ def rescale_energies_and_forces(model, datamodule, scale_factor):
     dataset = datamodule.train_dataset
     composition_layer = model.composition_layer
     numbers = torch.cat([data.numbers for data in dataset])
-    ptr = torch.cumsum(torch.tensor([0] + [data.num_nodes for data in dataset]), dim=0)
+    batch = torch.cat(
+        [
+            torch.repeat_interleave(torch.tensor([i]), data.num_nodes)
+            for i, data in enumerate(dataset)
+        ]
+    )
     compositions = torch.stack(
-        get_compositions_from_numbers(numbers, datamodule.unique_numbers, ptr)
+        get_compositions_from_numbers(numbers, datamodule.unique_numbers, batch)
     )
     composition_energies = composition_layer(compositions)
     composition_energies = composition_energies.squeeze()

torch_alchemical/utils/get_compositions_from_numbers.py
@@ -1,25 +1,19 @@
 import torch
-from typing import Union, Optional
+from typing import Optional
 
 
 def get_compositions_from_numbers(
-    numbers: Union[torch.Tensor, list[torch.Tensor]],
+    numbers: torch.Tensor,
     unique_numbers: list[int],
-    batch: Optional[torch.Tensor] = None,
+    batch: torch.Tensor,
     dtype: Optional[torch.dtype] = None,
 ) -> list[torch.Tensor]:
     compositions: list[torch.Tensor] = []
-    if isinstance(numbers, torch.Tensor):
-        assert batch is not None
-        device = numbers.device
-        _, counts = torch.unique(batch, return_counts=True)
-    else:
-        device = numbers[0].device
-        counts = torch.tensor([len(number) for number in numbers], device=device)
+    device = numbers.device
+    _, counts = torch.unique(batch, return_counts=True)
     dtype = dtype if dtype is not None else torch.float64
     ptr = torch.cat([torch.tensor([0], device=device), torch.cumsum(counts, dim=0)])
-    if isinstance(numbers, torch.Tensor):
-        numbers = [numbers[ptr[i] : ptr[i + 1]] for i in range(len(ptr) - 1)]
+    numbers = [numbers[ptr[i] : ptr[i + 1]] for i in range(len(ptr) - 1)]
     unique_numbers = torch.tensor(
         unique_numbers, dtype=numbers[0].dtype, device=numbers[0].device
     )