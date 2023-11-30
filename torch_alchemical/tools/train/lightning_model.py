e9e4e5a88607105d6474c28afc2b0e5505adc580
Arslan Mazitov <arslan.mazitov@epfl.ch>
2023-11-30 07:53:33 +0100

    Import fix

--- torch_alchemical/tools/train/lightning_model.py
+++ torch_alchemical/tools/train/lightning_model.py
@@ -2,7 +2,7 @@ import lightning.pytorch as pl
 import torch
 import os
 from torch_alchemical.nn import WeightedSSELoss, MAELoss
-from torch_alchemical.utils import get_autograd_forces
+from torch_alchemical.utils import get_autograd_forces, get_compositions_from_numbers
 from torch_alchemical.tools.logging.wandb import log_wandb_data
 
 
