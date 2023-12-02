7462ae0b97457ead73add1cc6553581ead7d48da
Arslan Mazitov <arslan.mazitov@epfl.ch>
2023-12-02 15:08:53 +0100

    Updated __init__.py in models

--- torch_alchemical/models/__init__.py
+++ torch_alchemical/models/__init__.py
@@ -1,5 +1,13 @@
 from .power_spectrum_model import PowerSpectrumModel
 from .bpps_model import BPPSModel
 from .alchemical_model import AlchemicalModel
+from .alchemical_gat import AlchemicalGAT
+from .alchemical_gcn import AlchemicalGCN
 
-__all__ = ["PowerSpectrumModel", "BPPSModel", "AlchemicalModel"]
+__all__ = [
+    "PowerSpectrumModel",
+    "BPPSModel",
+    "AlchemicalModel",
+    "AlchemicalGAT",
+    "AlchemicalGCN",
+]
