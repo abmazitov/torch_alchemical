ca5f5f03e59be8d1a6bcc4e52229700041f7c5c3
Arslan Mazitov <arslan.mazitov@epfl.ch>
2023-12-13 11:30:17 +0100

    Removed GAT and TransformerConv from available models temporarily

--- torch_alchemical/models/__init__.py
+++ torch_alchemical/models/__init__.py
@@ -1,13 +1,14 @@
 from .power_spectrum_model import PowerSpectrumModel
 from .bpps_model import BPPSModel
 from .alchemical_model import AlchemicalModel
-from .alchemical_gat import AlchemicalGAT
+
+# from .alchemical_gat import AlchemicalGAT
 from .alchemical_gcn import AlchemicalGCN
 
 __all__ = [
     "PowerSpectrumModel",
     "BPPSModel",
     "AlchemicalModel",
-    "AlchemicalGAT",
+    # "AlchemicalGAT",
     "AlchemicalGCN",
 ]
